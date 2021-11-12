c6a0af18996e558eb19963c76cf9be1b50dc4ba6
KhangNT1105 <KhangNT1105>
2021-11-12 14:48:00 +0700

    hoan thanh phan hieu

--- source.cpp
+++ source.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+};
 
 int main()
 {
@@ -13,6 +15,8 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+	int hieuSo = hieu(a,b);
+	cout <<"Hieu la:"<<hieuSo;
 	system("pause");
 	return 0;
 }
